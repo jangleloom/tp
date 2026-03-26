1dfeca53471561bf87bb44de5653c6628bfef1f5
janelle <lumjanelle@gmail.com>
2026-03-26 11:32:40 +0800

    Handle null aliasName in JsonAdaptedAlias deserialization
    Throw IllegalValueException with a descriptive message when aliasName
    is missing from JSON, consistent with null-checking in other JsonAdapted classes.

--- src/main/java/seedu/address/storage/JsonAdaptedAlias.java
+++ src/main/java/seedu/address/storage/JsonAdaptedAlias.java
@@ -11,6 +11,7 @@ import seedu.address.model.person.Alias;
  */
 class JsonAdaptedAlias {
 
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Alias's %s field is missing!";
     private final String aliasName;
 
     /**
@@ -39,6 +40,9 @@ class JsonAdaptedAlias {
      * @throws IllegalValueException if there were any data constraints violated in the adapted alias.
      */
     public Alias toModelType() throws IllegalValueException {
+        if (aliasName == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, "aliasName"));
+        }
         if (!Alias.isValidAlias(aliasName)) {
             throw new IllegalValueException(Alias.MESSAGE_CONSTRAINTS);
         }
